b7d902cf875d6c89df45580e99883813cda90800
Suporte 4 <suporte4@laser.local>
2024-10-24 09:18:55 -0300

    config: tailwilndcss

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{html,ts}", // Configurar os paths corretos para os templates Angular
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
